8be4c598de75cbfd9b891a700b8010c06cc9bf32
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-10-06 19:37:14 +0200

    Added ESLint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import dotenv from "dotenv"
+import process from 'process'
+import dotenv from 'dotenv'
 dotenv.config()
-import express from "express"
+import express from 'express'
 const app = express()
-import cors from "cors"
-import mysql from "mysql"
+import cors from 'cors'
+import mysql from 'mysql'
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
 
-const log_debug = false
-const log = (msg, debug) => {
-  process.env.NODE_ENV && console.log(msg)
+const log = (msg) => {
+	process.env.NODE_ENV && console.log(msg)
 }
 
 //  //  //  //  //
@@ -20,35 +20,35 @@ const log = (msg, debug) => {
 //
 //  //  //  //  //
 
-var db;
+var db
 var db_config = {
-  user: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_USER_DEV : process.env.REACT_APP_DB_USER_PRODUCTION,
-  host: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_HOST_DEV : process.env.REACT_APP_DB_HOST_PRODUCTION,
-  password: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_PASSWORD_DEV : process.env.REACT_APP_DB_PASSWORD_PRODUCTION,
-  database: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_NAME_DEV : process.env.REACT_APP_DB_NAME_PRODUCTION
+	user: process.env.NODE_ENV === 'dev' ? process.env.REACT_APP_DB_USER_DEV : process.env.REACT_APP_DB_USER_PRODUCTION,
+	host: process.env.NODE_ENV === 'dev' ? process.env.REACT_APP_DB_HOST_DEV : process.env.REACT_APP_DB_HOST_PRODUCTION,
+	password: process.env.NODE_ENV === 'dev' ? process.env.REACT_APP_DB_PASSWORD_DEV : process.env.REACT_APP_DB_PASSWORD_PRODUCTION,
+	database: process.env.NODE_ENV === 'dev' ? process.env.REACT_APP_DB_NAME_DEV : process.env.REACT_APP_DB_NAME_PRODUCTION
 }
 
 function db_connect() {
 
-  db = mysql.createConnection(db_config)
+	db = mysql.createConnection(db_config)
 
-  db.connect(function(err) {
-    if(err) {
-      setTimeout(db_connect(), 2000)
-      throw err
-    } else {
-      log("✅ Connected to db")
-    }
-  })
+	db.connect(function(err) {
+		if(err) {
+			setTimeout(db_connect(), 2000)
+			throw err
+		} else {
+			log('✅ Connected to db')
+		}
+	})
 
-  db.on('error', function(err) {
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-      log("🚧 Reconnecting to db...")
-      db_connect()
-    } else {
-      throw err
-    }
-  })
+	db.on('error', function(err) {
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			log('🚧 Reconnecting to db...')
+			db_connect()
+		} else {
+			throw err
+		}
+	})
 
 }
 
@@ -60,27 +60,27 @@ db_connect()
 //
 //  //  //  //  //
 
-app.get("/", (req, res) => {
-  res.send("✅ Wanna Hang Out server is online")
+app.get('/', (req, res) => {
+	res.send('✅ Wanna Hang Out server is online')
 })
 
 // Examples
 
-app.get("/api/hello", (req, res) => {
-  res.send("Hello world, his is working!")
+app.get('/api/hello', (req, res) => {
+	res.send('Hello world, his is working!')
 })
 
-app.get("/api/getAllEntities", (req, res) => {
-  db.query("SELECT * FROM entity", (err, result) => {
-    if (err) {
-      console.log(err)
-    }
-    res.send(result)
-  })
+app.get('/api/getAllEntities', (req, res) => {
+	db.query('SELECT * FROM entity', (err, result) => {
+		if (err) {
+			console.log(err)
+		}
+		res.send(result)
+	})
 })
 
 // Examples end
 
 app.listen(PORT, () => {
-  log("\nServer is up and running at port" + PORT)
+	log('\nServer is up and running at port' + PORT)
 })
