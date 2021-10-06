04bf31773aa67390e7b57838b2d96c8fcbb05cd3
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-10-06 18:58:59 +0200

    Db reconnection on timeout
    - Updated package.json
    - Added log() function to log info only when dev mode

--- index.js
+++ index.js
@@ -1,22 +1,74 @@
-import express from "express";
-const app = express();
-import cors from "cors";
-import mysql from "mysql";
-const PORT = 3001;
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+const app = express()
+import cors from "cors"
+import mysql from "mysql"
+const PORT = 3000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const db = mysql.createConnection({
-  user: "b3390b4687efaf",
-  host: "eu-cdbr-west-01.cleardb.com",
-  password: "ed3526bc",
-  database: "heroku_8710917fecf1cf0",
-});
+const log_debug = false
+const log = (msg, debug) => {
+  process.env.NODE_ENV && console.log(msg)
+}
+
+//  //  //  //  //
+//
+//  DB CONNECTION
+//
+//  //  //  //  //
+
+var db;
+var db_config = {
+  user: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_USER_DEV : process.env.REACT_APP_DB_USER_PRODUCTION,
+  host: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_HOST_DEV : process.env.REACT_APP_DB_HOST_PRODUCTION,
+  password: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_PASSWORD_DEV : process.env.REACT_APP_DB_PASSWORD_PRODUCTION,
+  database: process.env.NODE_ENV === "dev" ? process.env.REACT_APP_DB_NAME_DEV : process.env.REACT_APP_DB_NAME_PRODUCTION
+}
+
+function db_connect() {
+
+  db = mysql.createConnection(db_config)
+
+  db.connect(function(err) {
+    if(err) {
+      setTimeout(db_connect(), 2000)
+      throw err
+    } else {
+      log("✅ Connected to db")
+    }
+  })
+
+  db.on('error', function(err) {
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      log("🚧 Reconnecting to db...")
+      db_connect()
+    } else {
+      throw err
+    }
+  })
+
+}
+
+db_connect()
+
+//  //  //  //  //
+//
+//  API REST
+//
+//  //  //  //  //
+
+app.get("/", (req, res) => {
+  res.send("✅ Wanna Hang Out server is online")
+})
+
+// Examples
 
 app.get("/api/hello", (req, res) => {
-  res.send("Hello world, his is working!");
-});
+  res.send("Hello world, his is working!")
+})
 
 app.get("/api/getAllEntities", (req, res) => {
   db.query("SELECT * FROM entity", (err, result) => {
@@ -24,9 +76,11 @@ app.get("/api/getAllEntities", (req, res) => {
       console.log(err)
     }
     res.send(result)
-  });
-});
+  })
+})
+
+// Examples end
 
 app.listen(PORT, () => {
-  console.log("Server is up and running at port " + PORT);
-});
+  log("\nServer is up and running at port" + PORT)
+})
