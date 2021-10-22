6c65d14be357ceaeec3d77effc113a5c3ddc7f15
Rossalynna <53874270+Rossalyna@users.noreply.github.com>
2021-10-22 17:09:53 +0200

    createNewEntity

--- utils/entity.js
+++ utils/entity.js
@@ -28,21 +28,51 @@ export async function getEntityByID(db, entityID) {
 	})
 }
 
-export async function createNewActivity(db, req) {
-	var sqlInsert = 'INSERT INTO entity ( id_role, id_adress, nick, name, surname, description, mail, phone, pass, avatar) VALUES ( '
-	sqlInsert += req.query.id_role + ', '
-	sqlInsert += req.query.id_adress + ', '
+export async function createNewEntity(db, req) {
+	var sqlInsert = 'INSERT INTO entity ( id_role, id_address, nick, name, surname, description, mail, phone, pass, avatar) VALUES ( '
+	sqlInsert += parseInt(req.query.id_role) + ', '
+	sqlInsert += parseInt(req.query.id_address) + ', '
 	sqlInsert += req.query.nick + ', '
 	sqlInsert += req.query.name + ', '
 	sqlInsert += req.query.surname + ', '
 	sqlInsert += req.query.description + ', '
 	sqlInsert += req.query.mail + ', '
-	sqlInsert += req.query.phone + ', '
+	sqlInsert += parseInt(req.query.phone) + ', '
 	sqlInsert += req.query.pass + ', '
 	sqlInsert += req.query.avatar + ')'
 
 	log(sqlInsert)
-	
+	var idCreatedEntity = new Promise(resolve => {
+		db.query(sqlInsert, (err, result) => {
+			if (err) {
+				console.log(err)
+			}
+			resolve(JSON.stringify(result))
+		})
+	})
+
+	var tags_ent = req.query.tags_ent
+	tags_ent = tags_ent.split(',')
+	sqlInsert = 'INSERT INTO tags_ent ('
+	sqlInsert += 'id_entity, id_tags) VALUES ' 
+	for (const i in tags_ent) {
+		sqlInsert += '(' + await idCreatedEntity+ ', ' + tags_ent[i]
+		if ( i < (tags_ent.length - 1)){
+			sqlInsert += '), '
+		} else {
+			sqlInsert += '); '
+		}
+	}
+	new Promise(resolve => {
+		db.query(sqlInsert, (err, result) => {
+			if (err) {
+				console.log(err)
+			}
+			resolve(JSON.stringify(result))
+		})
+	})
+
+	return idCreatedEntity
 }
 
 //  //  //  //  //  //  //  //  //  //  //  //
